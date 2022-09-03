d8ca1f9df8a8244a6647f5a3baf3adbfcc2c2d7e
Alternatio <shinkarev0913@mail.ru>
2022-09-04 00:45:30 +0600

    start

---
+++ data/Home.data.tsx
@@ -0,0 +1,20 @@
+interface valuesOfGames {
+	name: string
+	date: string
+	techs: string
+}
+
+const homeData: valuesOfGames[] = [
+	{
+		name: 'monopoly',
+		date: '29.09.2022',
+		techs: 'ReactTS'
+	},
+	{
+		name: 'roulette',
+		date: '15.09.2022',
+		techs: 'ReactTS'
+	}
+]
+
+export default homeData
